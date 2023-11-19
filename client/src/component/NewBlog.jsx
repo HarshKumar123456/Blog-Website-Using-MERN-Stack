cc451e77deaf23d10eda23ef12b423863f20a464
Harsh <harshkumar92200@gmail.com>
2023-11-19 10:45:57 +0530

    added quill text editor component in create NewBlog.jsx page

--- client/src/component/NewBlog.jsx
+++ client/src/component/NewBlog.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, false] }],
+    ["bold", "italic"],
+    ["link", "blockquote", "code-block", "image"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    
+  ]
+};
+
 
 const NewBlog = ({ typeOfBlog, onCreateBlog }) => {
   const [blogTitle, setBlogTitle] = useState('');
   const [blogContent, setBlogContent] = useState('');
 
   const handleCreateBlog = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     if (!blogTitle || !blogContent) {
       alert('Please enter a title and content for the blog');
       return;
@@ -25,8 +38,8 @@ const NewBlog = ({ typeOfBlog, onCreateBlog }) => {
   return (
     <form onSubmit={handleCreateBlog} className="form-container">
       <input type="text" value={blogTitle} onChange={(e) => setBlogTitle(e.target.value)} placeholder="Enter title here" />
-      <textarea value={blogContent} onChange={(e) => setBlogContent(e.target.value)} placeholder="Save your memories...." />
-      <div className="button-container">
+      <ReactQuill modules={modules} onChange={setBlogContent} placeholder="Save your memories...." theme="snow" />
+      <div className="button-container mt-4">
         <button type="submit" className="btn btn-outline-success">Save</button>
         <button className="cancel-blog-creation btn btn-outline-danger">Cancel</button>
       </div>
