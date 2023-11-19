74517a5efb80c809617e3a4c2fc9d000249426f1
Harsh <harshkumar92200@gmail.com>
2023-11-19 11:16:39 +0530

    parsed html elements from string stored in database in BlogItem.jsx page

--- client/src/component/BlogItem.jsx
+++ client/src/component/BlogItem.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import parse from "html-react-parser";
 
-const BlogItem = ({ blog ,onDetail}) => {
+const blogStyle = {
+    "margin-top": '100px',
+    // "display": 'flex',
+    "minHeight": '100px',
+    "border": '1px solid black',
+    "borderRadius": '20px',
+    "padding": '40px',
+    "boxShadow": '4px 4px 10px #d9d9d9',
+    "background-color": "#ffffff",
+};
+
+
+
+const BlogItem = ({ blog, onDetail }) => {
     return (
-        <div className="blog-item">
-            <h2>{blog.title}</h2>
-            <p>{blog.content}</p>
+        <div className="blog-item m-2 p-2" style={blogStyle}>
+            <h2 style={{fontSize: "3.5rem"}}>{blog.title}....</h2>
+            <p>{parse(blog.content)}</p>
             <div className='container'>
                 <form>
                     <input type="hidden" name="blogId" value={blog._id} />
